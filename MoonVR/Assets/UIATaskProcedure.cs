377f9aea0637288f98b942f92ecafe32f9c4a16f
brussell03 <r.brussell03@gmail.com>
2023-05-21 23:15:21 -0500

    Task procedure and panel update. Removed unity camera clipping.

--- MoonVR/Assets/UIATaskProcedure.cs
+++ MoonVR/Assets/UIATaskProcedure.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.MixedReality.Toolkit.UI;
+﻿using JetBrains.Annotations;
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEngine.Rendering.DebugUI;
 
 public class UIATaskProcedure : MonoBehaviour
 {
     public TextMeshPro currentTask;
     public TextMeshPro titleLabel;
+    public TextMeshPro taskTitle;
+    public TextMeshPro stepIndicator;
     public Interactable previousTask;
     public Interactable nextTask;
     public Interactable doneButton;
@@ -17,13 +22,15 @@ public class UIATaskProcedure : MonoBehaviour
     public GameObject nextTaskIcon;
     public GameObject doneIcon;
 
-    public List<string> taskList = new List<string>();
-    public List<string> title = new List<string>();
+	public List<ProcedureTask> taskList = new List<ProcedureTask>();
+	public List<string> title = new List<string>();
     public GameObject UIA;
     public GameObject DCU;
     public GameObject[] switches;
     public GameObject dcuBatt;
-    private int counter = 0;
+    private int activeTaskIndex = 0;
+    private int activeSubtaskIndex = 0;
+    private int activeSubSubtaskIndex = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +38,7 @@ public class UIATaskProcedure : MonoBehaviour
         DCU.SetActive(false);
 
         previousTaskIcon.SetActive(false);
-        nextTask.OnClick.AddListener(() => ButtonPressed());
+        nextTask.OnClick.AddListener(() => NextTask());
         previousTask.OnClick.AddListener(() => ReverseTask());
         doneButton.OnClick.AddListener(() => TaskDone());
 
@@ -41,69 +48,126 @@ public class UIATaskProcedure : MonoBehaviour
             titleLabel.SetText(title[0]);
         }
 
-        currentTask.SetText(taskList[0]);
+        taskTitle.SetText(taskList[activeTaskIndex].taskTitle);
+        stepIndicator.SetText("Step: " + (activeTaskIndex + 1) + "." + (activeSubtaskIndex + 1) + " / " + taskList.Count);
+        currentTask.SetText(taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskStepTitle);
     }
 
+    public void UpdateTask()
+    {
+		if (activeTaskIndex < taskList.Count - 1)
+		{
+            if (taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskSubSteps.Count == 0)
+            {
+				taskTitle.SetText(taskList[activeTaskIndex].taskTitle);
+				stepIndicator.SetText("Step: " + (activeTaskIndex + 1) + "." + (activeSubtaskIndex + 1) + " / " + taskList.Count);
+				currentTask.SetText(taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskStepTitle);
+			}
+            else
+            {
+				taskTitle.SetText(taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskStepTitle);
+				stepIndicator.SetText("Step: " + (activeTaskIndex + 1) + "." + (activeSubtaskIndex + 1) + "." + (activeSubSubtaskIndex + 1) + " / " + taskList.Count);
+                currentTask.SetText(taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskSubSteps[activeSubSubtaskIndex]);
+			}
+		}
+		else
+		{
+			currentTask.SetText(taskList[activeTaskIndex].taskTitle);
+			taskTitle.SetText("");
+			stepIndicator.SetText("Step: " + (activeTaskIndex + 1) + " / " + taskList.Count);
+			nextTaskIcon.SetActive(false);
+			doneIcon.SetActive(true);
+		}
 
-    public void ButtonPressed()
+		if (activeTaskIndex == 0 && activeSubtaskIndex == 0 && activeSubSubtaskIndex == 0)
+		{
+			previousTaskIcon.SetActive(false);
+		}
+	}
+
+
+    public void NextTask()
     {
         nextTaskIcon.SetActive(true);
         previousTaskIcon.SetActive(true);
-        doneIcon.SetActive(true);
+        //doneIcon.SetActive(true);
 
-        counter = counter + 1;
-        if (counter < taskList.Count - 1)
+        if (activeSubSubtaskIndex < taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskSubSteps.Count - 1)
         {
-            currentTask.SetText(taskList[counter]);
-            doneIcon.SetActive(false);
+            activeSubSubtaskIndex++;
         }
-
         else
         {
-            nextTaskIcon.SetActive(false);
-            doneIcon.SetActive(true);
-            currentTask.SetText(taskList[taskList.Count - 1]);
-            counter = taskList.Count -1;
-        }
+            activeSubSubtaskIndex = 0;
+			if (activeSubtaskIndex < taskList[activeTaskIndex].taskSteps.Count - 1)
+			{
+				activeSubtaskIndex++;
+			}
+			else
+			{
+				activeTaskIndex++;
+				activeSubtaskIndex = 0;
+			}
+		}
 
-     
-    }
+        UpdateTask();
+	}
 
     public void ReverseTask()
     {
 
         nextTaskIcon.SetActive(true);
         previousTaskIcon.SetActive(true);
-        doneIcon.SetActive(true);
 
-        counter = counter - 1;
-        if (1 <= counter && counter < taskList.Count)
+        if (!((activeTaskIndex == 0 && activeSubtaskIndex == 0) && activeSubSubtaskIndex == 0))
         {
-            currentTask.SetText(taskList[counter]);
-            doneIcon.SetActive(false);
-        }
+			if (activeSubSubtaskIndex == 0)
+			{
+                if (activeSubtaskIndex > 0)
+                {
+                    activeSubtaskIndex--;
+                    if (taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskSubSteps.Count != 0)
+                    {
+						activeSubSubtaskIndex = taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskSubSteps.Count - 1;
+					}
+                    else
+                    {
+						activeSubSubtaskIndex = 0;
+					}
+				}
+                else
+                {
+                    if (activeTaskIndex > 0)
+                    {
+                        activeTaskIndex--;
+                        activeSubtaskIndex = taskList[activeTaskIndex].taskSteps.Count - 1;
+						if (taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskSubSteps.Count != 0)
+						{
+							activeSubSubtaskIndex = taskList[activeTaskIndex].taskSteps[activeSubtaskIndex].taskSubSteps.Count - 1;
+						}
+						else
+						{
+							activeSubSubtaskIndex = 0;
+						}
+					}
+                }
 
-        else if (counter < 1)
-        {
-            previousTaskIcon.SetActive(false);
-            doneIcon.SetActive(false);
-            currentTask.SetText(taskList[0]);
-            counter = 0;
-        }
+			}
+			else
+			{
+                activeSubSubtaskIndex--;
+			}
 
-        else
-        {
-            nextTaskIcon.SetActive(false);
-            currentTask.SetText(taskList[taskList.Count -1]);
-            counter = taskList.Count -1;
-            doneIcon.SetActive(false);
-        }
-    }
+			UpdateTask();
+		}
+	}
 
     public void TaskDone()
     {
-        counter = 0;
-        currentTask.SetText(taskList[counter]);
+        activeTaskIndex = 0;
+		activeSubtaskIndex = 0;
+		activeSubSubtaskIndex = 0;
+		UpdateTask();
         nextTaskIcon.SetActive(true);
         doneIcon.SetActive(false);
         previousTaskIcon.SetActive(false);
@@ -117,13 +181,13 @@ public class UIATaskProcedure : MonoBehaviour
         
         for (int i = 0; i < switches.Length; i++)
         {
-            if (counter == 0)
+            if (activeTaskIndex == 0)
             {
                 if (i == 0 || i == 5)
                 {
                     switches[i].SetActive(true);
                     
-                    switches[i].GetComponent<SwitchHologram>().enabled = false;
+                    switches[i].GetComponent<SwitchHologram>().enabled = true;
                   
 
                 }
@@ -135,9 +199,282 @@ public class UIATaskProcedure : MonoBehaviour
 
             }
 
-            else if (counter == 1)
+            else if (activeTaskIndex == 1)
             {
-                if (i == 1 || i == 3)
+				if (activeSubtaskIndex == 0)
+				{
+					if (i == 8)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+				else if (activeSubtaskIndex == 2)
+				{
+					if (i == 8)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+					}
+
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+			}
+
+            else if (activeTaskIndex == 2)
+            {
+                if (activeSubtaskIndex == 0)
+                {
+					if (i == 6 || i == 7)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+                else if (activeSubtaskIndex == 2)
+                {
+					if (i == 6 || i == 7)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+                else if (activeSubtaskIndex == 3)
+                {
+					if (i == 8)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+				else if (activeSubtaskIndex == 5)
+				{
+					if (i == 8)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+			}
+
+            else if (activeTaskIndex == 3)
+            {
+                if (activeSubtaskIndex == 0)
+                {
+					if (i == 6 || i == 7)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = true;
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+                else if (activeSubtaskIndex == 2)
+				{
+					if (i == 6 || i == 7)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = false;
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+
+			}
+
+
+            else if (activeTaskIndex == 4)
+            {
+				if (activeSubtaskIndex == 0)
+				{
+                    if (activeSubSubtaskIndex == 0)
+                    {
+						if (i == 2)
+						{
+							switches[i].SetActive(true);
+							switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+						}
+						else
+						{
+							switches[i].SetActive(false);
+						}
+					}
+                    else if (activeSubSubtaskIndex == 2)
+                    {
+						if (i == 2)
+						{
+							switches[i].SetActive(true);
+							switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+						}
+						else
+						{
+							switches[i].SetActive(false);
+						}
+					}
+				}
+				else if (activeSubtaskIndex == 1)
+				{
+					if (activeSubSubtaskIndex == 0)
+					{
+						if (i == 1)
+						{
+							switches[i].SetActive(true);
+							switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+						}
+
+						else
+						{
+							switches[i].SetActive(false);
+						}
+					}
+					else if (activeSubSubtaskIndex == 2)
+					{
+						if (i == 1)
+						{
+							switches[i].SetActive(true);
+							switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+						}
+
+						else
+						{
+							switches[i].SetActive(false);
+						}
+					}
+				}
+			}
+
+
+            else if (activeTaskIndex == 5)
+            {
+                if (activeSubtaskIndex == 0)
+                {
+					if (i == 9)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+                else if (activeSubtaskIndex == 1)
+                {
+                    if (activeSubSubtaskIndex == 0)
+                    {
+						if (i == 9)
+						{
+							switches[i].SetActive(true);
+							switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+						}
+						else
+						{
+							switches[i].SetActive(false);
+						}
+					}
+                    else if (activeSubSubtaskIndex == 2)
+                    {
+						if (i == 9)
+						{
+							switches[i].SetActive(true);
+							switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+						}
+						else
+						{
+							switches[i].SetActive(false);
+						}
+					}
+                }
+                else
+                {
+					if (i == 9)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+                
+            }
+
+            else if (activeTaskIndex == 6)
+            {
+                if (activeSubtaskIndex == 0)
+                {
+					if (i == 6 || i == 7)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = true;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+                else if (activeSubtaskIndex == 2)
+                {
+					if (i == 6 || i == 7)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = false;
+
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+                
+            }
+
+            else if (activeTaskIndex == 7)
+            {
+                if (i == 6)
                 {
                     switches[i].SetActive(true);
                     switches[i].GetComponent<SwitchHologram>().enabled = false;
@@ -150,69 +487,43 @@ public class UIATaskProcedure : MonoBehaviour
                 }
             }
 
-            else if (counter == 2)
+            else if (activeTaskIndex == 8)
             {
-                if (i == 6 || i == 7)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = false;
+				if (activeSubtaskIndex == 0)
+				{
+					if (i == 8)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = true;
 
-                }
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+				else if (activeSubtaskIndex == 2)
+				{
+					if (i == 8)
+					{
+						switches[i].SetActive(true);
+						switches[i].GetComponent<SwitchHologram>().enabled = false;
 
-                else
-                {
-                    switches[i].SetActive(false);
-                }
-            }
+					}
+					else
+					{
+						switches[i].SetActive(false);
+					}
+				}
+			}
 
-            else if (counter == 3)
+            else if (activeTaskIndex == 9)
             {
-                if (i == 8)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = true;
-
-                }
-
-                else
-                {
-                    switches[i].SetActive(false);
-                }
-            }
-
-
-            else if (counter == 4)
-            {
-                if (i == 8)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = false;
-
-                }
-
-                else
-                {
-                    switches[i].SetActive(false);
-                }
+				switches[i].SetActive(false);
             }
 
 
-            else if (counter == 5)
-            {
-                if (i == 8)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = false;
-
-                }
-
-                else
-                {
-                    switches[i].SetActive(false);
-                }
-            }
-
-            else if (counter == 6)
+            /*else if (activeTaskIndex == 10)
             {
                 if (i == 6)
                 {
@@ -227,7 +538,7 @@ public class UIATaskProcedure : MonoBehaviour
                 }
             }
 
-            else if (counter == 7)
+            else if (activeTaskIndex == 11)
             {
                 if (i == 6)
                 {
@@ -242,68 +553,7 @@ public class UIATaskProcedure : MonoBehaviour
                 }
             }
 
-            else if (counter == 8)
-            {
-                if (i == 8)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = true;
-
-                }
-
-                else
-                {
-                    switches[i].SetActive(false);
-                }
-            }
-
-            else if (counter == 9)
-            {
-                if (i == 8)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = false;
-
-                }
-
-                else
-                {
-                    switches[i].SetActive(false);
-                }
-            }
-
-
-            else if (counter == 10)
-            {
-                if (i == 6)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = true;
-
-                }
-
-                else
-                {
-                    switches[i].SetActive(false);
-                }
-            }
-
-            else if (counter == 11)
-            {
-                if (i == 6)
-                {
-                    switches[i].SetActive(true);
-                    switches[i].GetComponent<SwitchHologram>().enabled = false;
-
-                }
-
-                else
-                {
-                    switches[i].SetActive(false);
-                }
-            }
-
-            else if (counter == 12)
+            else if (activeTaskIndex == 12)
             {
                 UIA.SetActive(true);
                 DCU.SetActive(false);
@@ -321,7 +571,7 @@ public class UIATaskProcedure : MonoBehaviour
                 }
             }
 
-            else if (counter == 13)
+            else if (activeTaskIndex == 13)
             {
 
                 UIA.SetActive(false);
@@ -331,7 +581,7 @@ public class UIATaskProcedure : MonoBehaviour
 
             }
 
-            else if (counter == 14)
+            else if (activeTaskIndex == 14)
             {
 
                 UIA.SetActive(true);
@@ -348,9 +598,23 @@ public class UIATaskProcedure : MonoBehaviour
                 {
                     switches[i].SetActive(false);
                 }
-            }
+            }*/
 
 
         }
     }
+}
+
+[System.Serializable]
+public class ProcedureTask
+{
+    public string taskTitle;
+    public List<ProcedureTaskSteps> taskSteps;
+}
+
+[System.Serializable]
+public class ProcedureTaskSteps
+{
+    public string taskStepTitle;
+	public List<string> taskSubSteps;
 }
